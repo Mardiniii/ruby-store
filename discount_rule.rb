5357c25264e57b06ee06c2a521ebccb4456742be
mardiniii <sezama03@hotmail.com>
2016-10-10 00:07:58 -0500

    Define apply method to discoun rule class

--- discount_rule.rb
+++ discount_rule.rb
@@ -1,4 +1,6 @@
 class DiscountRule
+  attr_accessor :discount_per_item
+
   def initialize(code, quantity, discount_per_item)
     @code = code
     @quantity = quantity
@@ -6,6 +8,7 @@ class DiscountRule
   end
 
   def apply(items)
-
+    selected_items = items.select{ |i| i.code == @code}
+    selected_items.each{ |item| item.price -= @discount_per_item } if selected_items.size >= @quantity
   end
 end
