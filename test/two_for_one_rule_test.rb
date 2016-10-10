0a57185d14189affa969095903698240cdf9cf6a
mardiniii <sezama03@hotmail.com>
2016-10-10 01:08:45 -0500

    Define apply method into two_for_one_rule class

---
+++ test/two_for_one_rule_test.rb
@@ -0,0 +1,52 @@
+require 'test/unit'
+require_relative '../two_for_one_rule.rb'
+require_relative '../checkout.rb'
+
+class TwoForOneRuleTest < Test::Unit::TestCase
+  def setup
+    @rule = TwoForOneRule.new('VOUCHER')
+    @checkout = Checkout.new([@rule])
+    @store = @checkout.store
+    @voucher = @store.find('VOUCHER')
+    @tshirt = @store.find('TSHIRT')
+    @mug = @store.find('MUG')
+    @tshirt_one = Item.new(@tshirt.code, @tshirt.price)
+    @tshirt_two = Item.new(@tshirt.code, @tshirt.price)
+    @tshirt_three = Item.new(@tshirt.code, @tshirt.price)
+    @tshirt_four = Item.new(@tshirt.code, @tshirt.price)
+    @tshirt_five = Item.new(@tshirt.code, @tshirt.price)
+    @voucher_one = Item.new(@voucher.code, @voucher.price)
+    @voucher_two = Item.new(@voucher.code, @voucher.price)
+    @voucher_three = Item.new(@voucher.code, @voucher.price)
+    @voucher_four = Item.new(@voucher.code, @voucher.price)
+    @mug_one = Item.new(@mug.code, @mug.price)
+  end
+
+  def test_rule_application_without_items_to_apply_discount
+    items = [ @tshirt_one, @mug_one ]
+    @rule.apply(items)
+    assert_equal @tshirt_one.price, @tshirt_one.price, "Item price didn't change"
+    assert_equal @mug_one.price, @mug_one.price, "Item price didn't change"
+  end
+
+  def test_when_checkout_has_an_even_number_of_products_to_apply_two_for_one_discount
+    items = [@tshirt_one, @tshirt_two, @voucher_one, @voucher_two, @mug_one]
+    @rule.apply(items)
+    assert_equal @tshirt_one.price, @tshirt.price, "Item price didn't change"
+    assert_equal @tshirt_two.price, @tshirt.price, "Item price didn't change"
+    assert_equal @voucher_one.price, @voucher.price, "Item price didn't change"
+    assert_equal @voucher_two.price, 0, "Item price now is zero"
+    assert_equal @mug_one.price, @mug_one.price, "Item price didn't change"
+  end
+
+  def test_when_checkout_has_an_odd_number_of_products_to_apply_two_for_one_discount
+    items = [@tshirt_one, @tshirt_two, @voucher_one, @voucher_two, @voucher_three, @mug_one]
+    @rule.apply(items)
+    assert_equal @tshirt_one.price, @tshirt.price, "Item price didn't change"
+    assert_equal @tshirt_two.price, @tshirt.price, "Item price didn't change"
+    assert_equal @voucher_one.price, @voucher.price, "Item price didn't change"
+    assert_equal @voucher_two.price, 0, "Item price now is zero"
+    assert_equal @voucher_three.price, @voucher.price, "Item price didn't change"
+    assert_equal @mug_one.price, @mug_one.price, "Item price didn't change"
+  end
+end
--- two_for_one_rule.rb
+++ two_for_one_rule.rb
@@ -4,6 +4,11 @@ class TwoForOneRule
   end
 
   def apply(items)
-
+    selected_items = items.select{ |i| i.code == @code}
+    if selected_items.size >= 2
+      selected_items.each_slice(2) do |item, item_free|
+        item_free.price = 0 unless item_free.nil?
+      end
+    end
   end
 end
