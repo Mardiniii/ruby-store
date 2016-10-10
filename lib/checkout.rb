254344bbab9fb3a59a4c2548fa76ef91676927a5
mardiniii <sezama03@hotmail.com>
2016-10-10 13:07:16 -0500

    Fix typo error at checkout constructor

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -6,8 +6,8 @@ class Checkout
 
   def initialize(pricing_rules)
     @rules = pricing_rules
-    voucher = Product.new('VOUCHER', 'Cabify Voucher', 5.0),
-    tshirt = Product.new('TSHIRT', 'Cabify T-Shirt', 20.0),
+    voucher = Product.new('VOUCHER', 'Cabify Voucher', 5.0)
+    tshirt = Product.new('TSHIRT', 'Cabify T-Shirt', 20.0)
     mug = Product.new('MUG', 'Cafify Coffee Mug', 7.5)
     @store = Store.new(voucher, tshirt, mug)
     @items = []
