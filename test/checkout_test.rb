8f840a526bcd6e132cfc9ca4ae169936bb152113
mardiniii <sezama03@hotmail.com>
2016-10-10 01:25:02 -0500

    Add test cases to checkout_test.rb

--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -1,10 +1,14 @@
 require 'test/unit'
 require_relative '../checkout.rb'
+require_relative '../discount_rule.rb'
+require_relative '../two_for_one_rule.rb'
 
 class CheckoutTest < Test::Unit::TestCase
 
   def setup
-    @checkout = Checkout.new([])
+    @rule_one = TwoForOneRule.new('VOUCHER')
+    @rule_two = DiscountRule.new('TSHIRT', 3, 1.0)
+    @checkout = Checkout.new([ @rule_one, @rule_two ])
     @store = @checkout.store
   end
 
@@ -16,18 +20,39 @@ class CheckoutTest < Test::Unit::TestCase
     assert_equal @checkout.scan("CAR"), 'Sorry, This product code does not exist'
   end
 
-  def test_that_total_works_correcty
-    @checkout.scan('VOUCHER')
-    @checkout.scan('GLASSES')
-    @checkout.scan('INVITATION')
+  def test_that_total_works_correcty_without_apply_promotions_and_invalid_codes
     @checkout.scan('TSHIRT')
-    @checkout.scan('MUG')
     @checkout.scan('GLASSES')
+    @checkout.scan('VOUCHER')
+    @checkout.scan('INVITATION')
     @checkout.scan('MUG')
+    assert_equal 32.5, @checkout.total
+  end
+
+  def test_checkout_example_one
+    @checkout.scan('VOUCHER')
+    @checkout.scan('TSHIRT')
+    @checkout.scan('VOUCHER')
+    assert_equal 25.0, @checkout.total
+  end
+
+  def test_checkout_example_two
+    @checkout.scan('TSHIRT')
+    @checkout.scan('TSHIRT')
+    @checkout.scan('TSHIRT')
+    @checkout.scan('VOUCHER')
+    @checkout.scan('TSHIRT')
+    assert_equal 81.0, @checkout.total
+  end
+
+  def test_checkout_example_three
+    @checkout.scan('VOUCHER')
+    @checkout.scan('TSHIRT')
+    @checkout.scan('VOUCHER')
     @checkout.scan('VOUCHER')
     @checkout.scan('MUG')
-    @checkout.scan('VOUCHER')
-    @checkout.scan('PEN')
-    assert_equal 57.5, @checkout.total
+    @checkout.scan('TSHIRT')
+    @checkout.scan('TSHIRT')
+    assert_equal 74.5, @checkout.total
   end
 end
