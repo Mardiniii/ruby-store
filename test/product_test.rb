17e3d2f51415cca6b42befd744218de68ec483ce
mardiniii <sezama03@hotmail.com>
2016-10-09 21:50:47 -0500

    Move test to independent folder

--- test/product_test.rb
+++ test/product_test.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require_relative 'product'
+require_relative '../product.rb'
 
 class ProductTest < Test::Unit::TestCase
 
--- test/store_test.rb
+++ test/store_test.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require_relative 'store'
+require_relative '../store.rb'
 
 class StoreTest < Test::Unit::TestCase
 
