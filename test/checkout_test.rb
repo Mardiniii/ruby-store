23088888629bee5172d609fa875bcbd2c4ef120a
mardiniii <sezama03@hotmail.com>
2016-10-10 13:08:28 -0500

    Extend checkout test cases with one stage for the free_item_rule.rb

--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -2,13 +2,16 @@ require 'test/unit'
 require_relative '../lib/checkout.rb'
 require_relative '../lib/discount_rule.rb'
 require_relative '../lib/two_for_one_rule.rb'
+require_relative '../lib/free_item_rule.rb'
 
 class CheckoutTest < Test::Unit::TestCase
 
   def setup
     @rule_one = TwoForOneRule.new('VOUCHER')
     @rule_two = DiscountRule.new('TSHIRT', 3, 1.0)
+    @rule_three = FreeItemRule.new('MUG')
     @checkout = Checkout.new([ @rule_one, @rule_two ])
+    @checkout_with_free_mug = Checkout.new([ @rule_one, @rule_two, @rule_three ])
     @store = @checkout.store
   end
 
@@ -54,5 +57,19 @@ class CheckoutTest < Test::Unit::TestCase
     @checkout.scan('TSHIRT')
     @checkout.scan('TSHIRT')
     assert_equal 74.5, @checkout.total
+  end
+
+  def test_checkout_example_four_with_free_item_rule_for_mugs
+    @checkout_with_free_mug.scan('VOUCHER')
+    @checkout_with_free_mug.scan('TSHIRT')
+    @checkout_with_free_mug.scan('VOUCHER')
+    @checkout_with_free_mug.scan('VOUCHER')
+    @checkout_with_free_mug.scan('MUG')
+    @checkout_with_free_mug.scan('MUG')
+    @checkout_with_free_mug.scan('MUG')
+    @checkout_with_free_mug.scan('MUG')
+    @checkout_with_free_mug.scan('TSHIRT')
+    @checkout_with_free_mug.scan('TSHIRT')
+    assert_equal 67, @checkout_with_free_mug.total
   end
 end
