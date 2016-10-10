8d9a9f4606ed2de9f5e9e410ca4b74b7dd536a58
mardiniii <sezama03@hotmail.com>
2016-10-09 19:02:49 -0500

    Fix typo error and add to_s method using terminal-table gem

--- product.rb
+++ product.rb
@@ -1,9 +1,17 @@
+require 'terminal-table'
+
 class Product
   attr_reader :code, :name, :price
 
-  def initializa(code, name, price)
+  def initialize(code, name, price)
     @code = code
     @name = name
     @price = price
   end
+
+  def to_s
+    product = [ @code, @name, @price ]
+    table = Terminal::Table.new( headings: ['Code', 'Name', 'Price'], rows: [ product ])
+  end
 end
+
